767a74e623a727f23374c2b593a1d70b57f181ca
John Pennycook <john.pennycook@gmail.com>
2025-04-09 07:39:26 +0100

    Add ability to select icon style (#11)
    With this change, there are now three different icon styles that can be
    selected in the ProjectSettings:
    
    - Default: High-resolution 64x64 icons
    - Pixel: Low-resolution 16x16 icons
    - 1-bit: 1-bit equivalents of the 16x16 icons

--- addons/input_prompts/plugin.gd
+++ addons/input_prompts/plugin.gd
@@ -7,57 +7,62 @@ var inspector_plugin = preload("res://addons/input_prompts/inspector_plugin.gd")
 
 
 func _enter_tree():
-	add_autoload_singleton("PromptManager", "res://addons/input_prompts/input_prompt_manager.gd")
-	add_inspector_plugin(inspector_plugin)
-
 	if Engine.is_editor_hint():
 		var deadzone_setting := "addons/input_prompts/joypad_detection_deadzone"
 		if not ProjectSettings.has_setting(deadzone_setting):
 			ProjectSettings.set_setting(deadzone_setting, 0.5)
-			ProjectSettings.set_initial_value(deadzone_setting, 0.5)
-			ProjectSettings.set_as_basic(deadzone_setting, true)
+		ProjectSettings.set_initial_value(deadzone_setting, 0.5)
+		ProjectSettings.set_as_basic(deadzone_setting, true)
+		ProjectSettings.add_property_info(
+			{
+				"name": deadzone_setting,
+				"type": TYPE_FLOAT,
+				"hint": PROPERTY_HINT_RANGE,
+				"hint_string": "0,1"
+			}
+		)
+
+		var style_setting := "addons/input_prompts/icons/style"
+		if not ProjectSettings.has_setting(style_setting):
+			ProjectSettings.set_setting(style_setting, "Default")
+		ProjectSettings.set_initial_value(style_setting, "Default")
+		ProjectSettings.set_as_basic(style_setting, true)
+		ProjectSettings.add_property_info(
+			{
+				"name": style_setting,
+				"type": TYPE_STRING,
+				"hint": PROPERTY_HINT_ENUM,
+				"hint_string": "Default,1-bit,Pixel"
+			}
+		)
+
+		var icon_settings = [
+			"addons/input_prompts/icons/keyboard",
+			"addons/input_prompts/icons/mouse_buttons",
+			"addons/input_prompts/icons/joypad_buttons/nintendo",
+			"addons/input_prompts/icons/joypad_buttons/sony",
+			"addons/input_prompts/icons/joypad_buttons/xbox",
+			"addons/input_prompts/icons/joypad_motion/nintendo",
+			"addons/input_prompts/icons/joypad_motion/sony",
+			"addons/input_prompts/icons/joypad_motion/xbox",
+		]
+		for setting in icon_settings:
+			if not ProjectSettings.has_setting(setting):
+				ProjectSettings.set_setting(setting, "")
+			ProjectSettings.set_initial_value(setting, "")
 			ProjectSettings.add_property_info(
 				{
-					"name": deadzone_setting,
-					"type": TYPE_FLOAT,
-					"hint": PROPERTY_HINT_RANGE,
-					"hint_string": "0,1"
+					"name": setting,
+					"type": TYPE_STRING,
+					"hint": PROPERTY_HINT_FILE,
+					"hint_string": "*.tres,*.res"
 				}
 			)
 
-		var icon_settings = {
-			"addons/input_prompts/icons/keyboard":
-			"res://addons/input_prompts/key_prompt/keys.tres",
-			"addons/input_prompts/icons/mouse_buttons":
-			"res://addons/input_prompts/mouse_button_prompt/buttons.tres",
-			"addons/input_prompts/icons/joypad_buttons/nintendo":
-			"res://addons/input_prompts/joypad_button_prompt/nintendo.tres",
-			"addons/input_prompts/icons/joypad_buttons/sony":
-			"res://addons/input_prompts/joypad_button_prompt/sony.tres",
-			"addons/input_prompts/icons/joypad_buttons/xbox":
-			"res://addons/input_prompts/joypad_button_prompt/xbox.tres",
-			"addons/input_prompts/icons/joypad_motion/nintendo":
-			"res://addons/input_prompts/joypad_motion_prompt/nintendo.tres",
-			"addons/input_prompts/icons/joypad_motion/sony":
-			"res://addons/input_prompts/joypad_motion_prompt/sony.tres",
-			"addons/input_prompts/icons/joypad_motion/xbox":
-			"res://addons/input_prompts/joypad_motion_prompt/xbox.tres",
-		}
-		for setting in icon_settings.keys():
-			var value = icon_settings[setting]
-			if not ProjectSettings.has_setting(setting):
-				ProjectSettings.set_setting(setting, value)
-				ProjectSettings.set_initial_value(setting, value)
-				ProjectSettings.add_property_info(
-					{
-						"name": setting,
-						"type": TYPE_STRING,
-						"hint": PROPERTY_HINT_FILE,
-						"hint_string": "*.tres,*.res"
-					}
-				)
-
 		ProjectSettings.save()
+
+	add_autoload_singleton("PromptManager", "res://addons/input_prompts/input_prompt_manager.gd")
+	add_inspector_plugin(inspector_plugin)
 
 
 func _exit_tree():
